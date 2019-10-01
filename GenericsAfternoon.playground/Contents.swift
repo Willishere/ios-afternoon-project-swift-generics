5d90ec7e755ddc0d1ddb1d518d9c9dcf5e3ee053
William Chen <chen.will7@gmail.com>
2019-10-01 12:45:41 -0700

    some deletions

--- GenericsAfternoon.playground/Contents.swift
+++ GenericsAfternoon.playground/Contents.swift
@@ -1,3 +1,39 @@
 import UIKit
 
 var str = "Hello, playground"
+
+struct CountedSet<Element: Hashable>{
+    private (set) var elements: [Element]
+    
+    mutating func insertion(_ element: Element) -> Int?{
+        elements.append(element)
+        return elements.count
+    }
+    
+    mutating func deletion() -> Element? {
+        if elements.count > 0 {
+            return elements.removeFirst()
+        }else{
+            return nil
+        }
+    
+    }
+    
+    subscript(_ index: Element) -> Int? {
+    
+        get {
+            if elements.index(after: index.hashValue) != nil{
+                elements.index(after: index.hashValue)
+                return index.hashValue
+            }else{
+                return 0
+            }
+         
+            
+        }
+        
+        
+    }
+    
+    
+}
